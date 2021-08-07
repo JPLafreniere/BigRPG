9161c0e05a8cbae856d5f0efc6cff2208eab7406
JPLafreniere <JPLafreniere@gmail.com>
2021-08-07 12:34:30 -0700

    Small naming updates

--- Source/BigRPG/BigRPGGameMode.cpp
+++ Source/BigRPG/BigRPGGameMode.cpp
@@ -7,7 +7,7 @@
 ABigRPGGameMode::ABigRPGGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/BigRPGCharacter_BP"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
